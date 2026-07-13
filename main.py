4ece30a3ed77f3227f0e26e8ce4ec71860a10bb4
GregWatson <gwatson@gmail.com>
2026-07-13 16:47:00 -0700

    Added code to find triangles for each side. P9_backside piece 3 fails corner detect.

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ from fl_remove_background import fl_remove_background
 from fl_pad_and_scale import fl_pad_and_scale
 from find_tabs import find_tabs
 from find_blanks import find_blank_lines
+from find_triangles import find_triangles_from_corners
 
 def main():
     parser = argparse.ArgumentParser(description="Piece Project CLI")
@@ -97,6 +98,8 @@ def main():
             rotated_image_grey = cv2.warpAffine(piece_image, rotation_matrix, (500, 500))
             rotated_image = cv2.cvtColor(rotated_image_grey, cv2.COLOR_GRAY2BGR)
             w, h = rotated_image_grey.shape[1], rotated_image_grey.shape[0]
+            rotated_edges = edges.copy()
+            rotated_edges = cv2.warpAffine(rotated_edges, rotation_matrix, (w, h))
 
             # Rotate the lines we found as well for debugging purposes, and draw them on the rotated image. 
             # This will help us see if the rotation is correct and if the lines are aligned with the edges 
@@ -127,13 +130,28 @@ def main():
             # for _, point, angle_rad in corners:
             #     cv2.circle(rotated_image, (int(point[0]), int(point[1])), 10, (0, 0, int(255*angle_rad/(2*np.pi))), -1)
             # show_image(rotated_image, str=f"Corners {idx+1}", max=1000, wait_for_key=True)
-            # print(f"Corners: {corners}")    
+            # print(f"Corners: {corners}")
+
+            # Get triangles formed by corners and the point furthest from the line between 2 corners.
+            # Operate on rotated edge image.
+            triangles = find_triangles_from_corners(rotated_edges, corners, debug=args.debug)
+
+            unrotated_corner_points = []
+            for row in corners:
+                unrotated_row = []
+                for corner in row:
+                    if corner:
+                        _, point, _ = corner
+                        unrotated_point = rotate_point(point, (cx, cy), -rotation_angle_rad)
+                        pt_in_orig = inverse_transform_fn(map(int, unrotated_point))
+                        unrotated_row.append( (int(pt_in_orig[0]), int(pt_in_orig[1])) )
+                    else:
+                        unrotated_row.append(False)
+                unrotated_corner_points.append(unrotated_row)
 
             # Also add corners to the original image.
-            for _, point, _ in corners:
-                unrotated_point = rotate_point(point, (cx, cy), -rotation_angle_rad)
-                pt_in_orig = inverse_transform_fn(map(int, unrotated_point))
-                cv2.circle(resized_image, (int(pt_in_orig[0]), int(pt_in_orig[1])), 10, (0, 0, 255), -1)
+            for corner_point in [ col for row in unrotated_corner_points for col in row if col]:
+                cv2.circle(resized_image, corner_point, 10, (0, 0, 255), -1)
 
             # Also add tab points to original image
             for line in tab_lines:
@@ -148,7 +166,7 @@ def main():
                 cv2.line(resized_image, (int(o_pts[0][0]), int(o_pts[0][1])), (int(o_pts[1][0]), int(o_pts[1][1])), (250, 0, 200), 5)
 
             # Put number on piece
-            cv2.putText(resized_image, f"{idx}", (piece['centroid'][0] - 20, piece['centroid'][1] + 20) , cv2.FONT_HERSHEY_SIMPLEX, 3.0, (100, 100, 100), 3)
+            cv2.putText(resized_image, f"{idx}", (piece['centroid'][0] - 20, piece['centroid'][1] + 20) , cv2.FONT_HERSHEY_SIMPLEX, 3.0, (100, 100, 100), 5)
 
             # Show centroid
             cv2.circle(resized_image, (int(piece['centroid'][0]), int(piece['centroid'][1])), 5, (200,30,30), 6)
